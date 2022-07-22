1976442e28ec2f3aa51f5d4cc541ba26d9b69250
Alany Fernandes <alanymara@gmail.com>
2022-07-22 12:15:01 -0300

    feat: validation of qtdeAtivo done

--- src/services/investimentos.service.js
+++ src/services/investimentos.service.js
@@ -3,11 +3,17 @@ const investimentosModel = require('../models/investimentos.model');
 const comprarAtivos = async ({ codAtivo, qtdeAtivo, codCliente }) => {
   const ativo = await getAtivosByCod(codAtivo);
   const cliente = await getClientesByCod(codCliente);
-  
+
   if (ativo.length === 0 || cliente.length === 0) {
     const erro = { status: 400, message: 'Codigo do ativo ou do cliente inválido'};
     throw erro;
   }
+
+  if (ativo[0].qtdeAtivo < qtdeAtivo) {
+    const erro = { status: 400, message: 'Quantidade de ativos ultrapassa a disponibilidade'};
+    throw erro;
+  }
+
   const result = await investimentosModel.comprarAtivos(codAtivo, qtdeAtivo, codCliente)
   return result;
 }
