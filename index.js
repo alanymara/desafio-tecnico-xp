b18dccfa3be3ac593350e2cf3ae39987e10a1ad6
Alany Fernandes <alanymara@gmail.com>
2022-07-22 12:07:03 -0300

    feat: middleware of error done, validation of cliente and ativo exists done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const investimentosRouters = require('./src/routers/investimentos.router');
+const error = require('./src/middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', investimentosRouters)
+
+app.use(error);
 
 const port = process.env.PORT;
 
