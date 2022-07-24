5777aa1ee6b5a987db9eb2cfa16d8075d8560b78
Alany Fernandes <alanymara@gmail.com>
2022-07-24 15:09:45 -0300

    feat: implementation of functions geradorJWTToken and autenticacaoToken

---
+++ src/utils/JWTToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET }= process.env;
+
+const jwtconfig = {
+  expiresIn: '10d',
+  algorithm: 'HS256',
+};
+
+const geradorJWTToken = (informacoes) => jwt.sign(informacoes, JWT_SECRET, jwtconfig);
+
+const autenticacaoToken = async (token) => {
+  if (!token) {
+    const erro = { status: 401, message: 'Token não encontrado' };
+    throw erro;
+  };
+  try {
+    const verificacao = await jwt.verify(token, JWT_SECRET, jwtconfig);
+    return verificacao;
+  } catch (error) {
+    const erro = { status: 401, message: 'Token inspirado ou inválido' };
+    throw erro;
+  }
+};
+
+module.exports = {
+  geradorJWTToken,
+  autenticacaoToken,
+}
