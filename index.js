8a10f4cb10abe18beff7e4edb2acb240899e1cfa
Alany Fernandes <alanymara@gmail.com>
2022-07-24 19:42:49 -0300

    fix: uptade port of index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/', loginRouters);
 
 app.use(error);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`escutando a porta ${port}`)
